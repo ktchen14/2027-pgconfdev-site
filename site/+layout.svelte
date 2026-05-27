90fba5fafc3d6b3d6be34c889270c0b496fcebcd
Kaiting Chen <ktchen14@gmail.com>
2026-05-27 19:15:11 -0400

    Move font import to site.css

--- site/+layout.svelte
+++ site/+layout.svelte
@@ -1,8 +1,4 @@
 <script lang="ts">
-  import "@fontsource/montserrat/400-italic.css";
-  import "@fontsource/montserrat/400.css";
-  import "@fontsource/montserrat/700-italic.css";
-  import "@fontsource/montserrat/700.css";
   import montserratLatin400NormalWoff2 from "@fontsource/montserrat/files/montserrat-latin-400-normal.woff2?url";
   import montserratLatin700NormalWoff2 from "@fontsource/montserrat/files/montserrat-latin-700-normal.woff2?url";
   import "./site.css";
