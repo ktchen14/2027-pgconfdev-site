c99bc37da5dd686777b72b29eda2c01f73e21890
Kaiting Chen <ktchen14@gmail.com>
2026-06-02 17:52:37 -0400

    Smaller logo

--- common/logo/PGConf.svelte
+++ common/logo/PGConf.svelte
@@ -4,7 +4,7 @@
   const { color = "currentColor", ...rest } = $props();
 </script>
 
-<svg fill={color} viewBox="0 0 1629 272.5" {...rest}>
+<svg fill={color} viewBox="0 0 1464.2 411.4" {...rest}>
   <title>PGConf.dev</title>
   <use href="{logo}#root" />
 </svg>
