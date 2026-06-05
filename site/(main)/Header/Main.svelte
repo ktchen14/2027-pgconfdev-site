546cfb60d7eaf255809d420c325d54483a69dec3
Kaiting Chen <ktchen14@gmail.com>
2026-06-05 15:02:00 -0400

    tweak

--- site/(main)/Header/Main.svelte
+++ site/(main)/Header/Main.svelte
@@ -35,6 +35,15 @@
     [data-search] > & {
       display: none;
     }
+
+    :global(button) {
+      padding-inline: 0.75em;
+      margin-inline: -0.75em;
+    }
+
+    :global(li) {
+      margin-block: 0.5em;
+    }
   }
 
   menu {
