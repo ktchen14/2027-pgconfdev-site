e367691e1d37fe22855d7b6111a3884cfda3d405
Kaiting Chen <ktchen14@gmail.com>
2026-06-07 12:17:23 -0400

    Add none rule

--- uno.config.js
+++ uno.config.js
@@ -2,4 +2,18 @@ import { defineConfig } from "unocss";
 
 export default defineConfig({
   presets: [],
+  rules: [
+    ["none", { display: "none" }],
+  ],
+  variants: [
+    (matcher) => {
+      const result = matcher.match(/^(.+)\[(\d+)?-(\d+)?\]$/);
+      if (!result) return;
+      const [, rule, minimum, maximum] = result;
+      const q = [];
+      if (minimum) q.push(`(width >= ${minimum}rem)`);
+      if (maximum) q.push(`(width < ${maximum}rem)`);
+      return { matcher: rule, parent: `@media ${q.join(" and ")}` };
+    },
+  ],
 });
