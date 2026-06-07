3c879cfb8bc9d119c8bffd999b8076612e578a80
Kaiting Chen <ktchen14@gmail.com>
2026-06-07 12:09:24 -0400

    No custom selectors

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,9 @@
 import { enhancedImages } from "@sveltejs/enhanced-img";
 import { sveltekit } from "@sveltejs/kit/vite";
-import postcssPresetEnv from "postcss-preset-env";
 import UnoCSS from "unocss/vite";
 import { defineConfig } from "vite";
 import lucidePreprocess from "vite-plugin-lucide-preprocess";
 
 export default defineConfig({
-  css: {
-    postcss: {
-      plugins: [
-        postcssPresetEnv({
-          features: {
-            "custom-selectors": true,
-          },
-        }),
-      ],
-    },
-  },
   plugins: [UnoCSS(), enhancedImages(), lucidePreprocess(), sveltekit()],
 });
