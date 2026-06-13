87083c65d221e163a58dd5f1921b68d0c3b14b93
Kaiting Chen <ktchen14@gmail.com>
2026-06-13 08:33:05 -0400

    Link

---
+++ common/Link.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { page } from "$app/state";
+
+  const { href, children, ...rest } = $props();
+
+  const to = $derived(new URL(href, page.url));
+
+  const ariaCurrent = $derived(
+    to.pathname === page.url.pathname ? "page" : undefined,
+  );
+
+  const target = $derived(to.origin === page.url.origin ? undefined : "_blank");
+</script>
+
+<a aria-current={ariaCurrent} {href} {target} {...rest}>
+  {@render children()}
+</a>
