19fec45ac57bc84df8bcf9caa961b258a5d758b2
Kaiting Chen <ktchen14@gmail.com>
2026-06-13 09:10:50 -0400

    Add link test

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { enhancedImages } from "@sveltejs/enhanced-img";
 import { sveltekit } from "@sveltejs/kit/vite";
 import { playwright } from "@vitest/browser-playwright";
 import postcssPresetEnv from "postcss-preset-env";
+// @ts-expect-error no type module
 import postcssSize from "postcss-size";
 import UnoCSS from "unocss/vite";
 import lucidePreprocess from "vite-plugin-lucide-preprocess";
