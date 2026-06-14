79f1440e739ada8476e530c05a82d82ab44d12a1
Kaiting Chen <ktchen14@gmail.com>
2026-06-14 10:27:04 -0400

    move

--- site/internal/readme/+layout.svelte
+++ site/internal/readme/+layout.svelte
@@ -20,6 +20,9 @@
     <p class="over">CSS Utilities</p>
     <ul>
       <li>
+        <Link href={resolve("/internal/readme/page-layout")}>Page layout</Link>
+      </li>
+      <li>
         <Link href={resolve("/internal/readme/text")}>Text</Link>
       </li>
       <li>
@@ -27,9 +30,6 @@
       </li>
       <li>
         <Link href={resolve("/internal/readme/layout")}>Layout</Link>
-      </li>
-      <li>
-        <Link href={resolve("/internal/readme/page-layout")}>Page layout</Link>
       </li>
     </ul>
   </nav>
