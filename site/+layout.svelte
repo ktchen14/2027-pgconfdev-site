cf5a7103a37e984004b429a99931fbc1f40d3e61
Kaiting Chen <ktchen14@gmail.com>
2026-06-16 18:26:41 -0400

    Fonts

--- site/+layout.svelte
+++ site/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
-  import newsreaderLatin400NormalWoff2 from "@fontsource/newsreader/files/newsreader-latin-400-normal.woff2?url";
-  import newsreaderLatin500NormalWoff2 from "@fontsource/newsreader/files/newsreader-latin-500-normal.woff2?url";
+  import publicSansLatinWghtNormalWoff2 from "@fontsource-variable/public-sans/files/public-sans-latin-wght-normal.woff2?url";
+  import newsreaderLatinOpszNormalWoff2 from "@fontsource-variable/newsreader/files/newsreader-latin-opsz-normal.woff2?url";
   import "./site.css";
   import "virtual:uno.css";
 
@@ -12,14 +12,14 @@
     rel="preload"
     as="font"
     type="font/woff2"
-    href={newsreaderLatin400NormalWoff2}
+    href={publicSansLatinWghtNormalWoff2}
     crossorigin="anonymous"
   />
   <link
     rel="preload"
     as="font"
     type="font/woff2"
-    href={newsreaderLatin500NormalWoff2}
+    href={newsreaderLatinOpszNormalWoff2}
     crossorigin="anonymous"
   />
 </svelte:head>
