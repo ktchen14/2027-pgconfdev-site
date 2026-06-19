fe4c30d652ee43440cadb258c4d12e7baaeb3e72
Kaiting Chen <ktchen14@gmail.com>
2026-06-19 12:33:40 -0400

    Fix hooks

--- hook.ts
+++ hook.ts
@@ -1,10 +1,6 @@
 if (typeof globalThis.Temporal === "undefined") {
   const { Temporal, toTemporalInstant } = await import("temporal-polyfill");
-
-  // @ts-expect-error - polyfilling globalThis
   globalThis.Temporal = Temporal;
-
-  // @ts-expect-error - patching Date.prototype
   Date.prototype.toTemporalInstant = toTemporalInstant;
 }
 
